2779673f04566e95bb839a6a3404dd5a5cd15802
hsatvidkar <hsatvidkar@gmail.com>
2017-06-20 17:44:16 +0530

    Letzoutsource Theme
    Letzoutsource Theme

---
+++ main.js
@@ -0,0 +1,58 @@
+/* ---------------------------------------------
+ common scripts
+ --------------------------------------------- */
+
+;(function () {
+
+    "use strict"; // use strict to start
+
+	/* ---------------------------------------------
+		tb preloader init
+     --------------------------------------------- */
+    $(window).on('load', function() {
+        $("body").imagesLoaded(function(){
+            $(".tb-preloader-wave").fadeOut();
+            $("#tb-preloader").delay(200).fadeOut("slow").remove();
+        });
+    });
+	
+	/* ---------------------------------------------
+		Sticky Header
+     --------------------------------------------- */
+	$(window).scroll(function() {
+		if ($(this).scrollTop() > 0){  
+			$('header').addClass("sticky");
+		}
+		else{
+			$('header').removeClass("sticky");
+		}
+	});
+	
+	/* ---------------------------------------------
+		Main Menu
+     --------------------------------------------- */
+	$(document).ready(function () {
+		$('.navbar-nav li.expanded').removeClass('expanded');
+		$('.navbar-nav li.first').removeClass('first');
+		$('.navbar-nav li.leaf').removeClass('leaf');
+		$('.navbar-nav li.last').removeClass('last');
+		$('.navbar-nav li.active-trail').removeClass('active-trail');
+	});
+		
+	/* ---------------------------------------------
+		Hamburger Animation
+     --------------------------------------------- */
+	$(document).ready(function () {
+		$(".navbar-toggle").on("click", function () {
+			$(this).toggleClass("active");
+		});
+		
+	});
+	
+	/* ---------------------------------------------
+		WOW init
+     --------------------------------------------- */
+    if (typeof WOW == "function")
+    new WOW().init();
+	
+})(jQuery);
